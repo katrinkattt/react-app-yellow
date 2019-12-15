4be78d4db64ff6dd33d01e0046cc986cfc2a921b
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-15 22:00:32 +0500

    tr

---
+++ app-lending/yell-app/src/components/secondContainer.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+export default class SecondContainer extends Component {
+    render(){
+        return(
+            <div className="container">
+                <div className="title_container title__second">Our Services</div>
+                <div className="title2__"></div>
+                <div className="collumn">
+                    <table>
+                        <tr>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
