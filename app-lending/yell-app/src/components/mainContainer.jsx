bfbde6dc600403a14d3172b86471f7402de3d2be
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-15 21:00:01 +0500

    h

--- app-lending/yell-app/src/components/mainContainer.jsx
+++ app-lending/yell-app/src/components/mainContainer.jsx
@@ -5,6 +5,10 @@ export default class MainContainer extends Component {
         return(
             <div className="main__container">
                 <div className="title__main">
+                    <div className="home__line">
+                        <div className="line__home"></div>
+                        <p className="title__home">HOME</p>
+                        </div>
                     <p>Creative agency </p>
                     <p className="second__title">Digital design</p>
                     </div>
