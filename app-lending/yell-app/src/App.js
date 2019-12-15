ea93ffe0c7c31ad23e6f8a782936e2a2e0a3d9fb
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-15 23:02:28 +0500

    +menu

--- app-lending/yell-app/src/App.js
+++ app-lending/yell-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import MainContainer from './components/mainContainer';
 import OurService from './components/ourService';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <div className="App">
       <MainContainer />
       <OurService />
+      <Cards />
     </div>
   );
 }
---
+++ app-lending/yell-app/src/components/menu.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class Menu extends Component {
+    render(){
+        return(
+            <div className="menu">
+                <p>Home</p>
+                <p>Our Service</p>
+                <p>Bye</p>
+                <p>About</p>
+            </div>
+        )
+    }
+}
