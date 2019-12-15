be4e736756e08967a3da087f12fe03dcd04b71f2
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-15 20:44:08 +0500

    f

--- app-lending/yell-app/src/components/mainContainer.jsx
+++ app-lending/yell-app/src/components/mainContainer.jsx
@@ -3,9 +3,13 @@ import React, {Component} from 'react';
 export default class MainContainer extends Component {
     render(){
         return(
-            <div className="MainContainer">
-                <div className="titleMain">Tests</div>
-                <div className="titleMini">kak variant</div>
+            <div className="main__container">
+                <div className="title__main">
+                    <p>Creative agency </p>
+                    <p className="second__title">Digital design</p>
+                    </div>
+                <div className="title__mini">kak variant</div>
+                <div className="grey__block"></div>
             </div>
         )
     }
