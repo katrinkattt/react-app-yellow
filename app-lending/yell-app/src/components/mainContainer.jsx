1cada21a775a1903d1950c79a2fb8b62a153f6c7
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-15 19:10:30 +0500

    starty

---
+++ app-lending/yell-app/src/components/mainContainer.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class MainContainer extends Component {
+    render(){
+        return(
+            <div className="MainContainer">
+                <div className="titleMain">Tests</div>
+                <div className="titleMini">kak variant</div>
+            </div>
+        )
+    }
+}
