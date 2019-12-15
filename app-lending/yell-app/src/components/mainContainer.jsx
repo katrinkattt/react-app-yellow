e799475d5ae68e7f0b42aba3a5bc91f23333d419
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-15 23:17:02 +0500

    m

--- app-lending/yell-app/src/components/mainContainer.jsx
+++ app-lending/yell-app/src/components/mainContainer.jsx
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import Menu from './menu'
 
 export default class MainContainer extends Component {
     render(){
         return(
             <div className="main__container">
+                <Menu />
                 <div className="title__main">
                     <div className="home__line">
                         <div className="line__home"></div>
--- app-lending/yell-app/src/components/menu.jsx
+++ app-lending/yell-app/src/components/menu.jsx
@@ -4,10 +4,7 @@ export default class Menu extends Component {
     render(){
         return(
             <div className="menu">
-                <p>Home</p>
-                <p>Our Service</p>
-                <p>Bye</p>
-                <p>About</p>
+                <div><a>Home</a> <a>Our Service</a> <a>Bye</a>  <a>About</a></div>
             </div>
         )
     }
