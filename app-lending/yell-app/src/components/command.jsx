28670bcdf4d492bd1cff0f765b1de50edef1a6fc
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-16 21:14:09 +0500

    style

--- app-lending/yell-app/src/components/command.jsx
+++ app-lending/yell-app/src/components/command.jsx
@@ -4,6 +4,7 @@ export default class Command extends Component {
     render(){
         return(
             <div className="container">
+                <div className="title_container">Our command</div>
                 <div className="command__main">
                     <div className="person"></div>
                     <div className="person"></div>
