b1bbb07f706848ad9c6ef8370a19af6b5a680a66
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-16 16:00:27 +0500

    color

--- app-lending/yell-app/src/components/Cards.jsx
+++ app-lending/yell-app/src/components/Cards.jsx
@@ -6,14 +6,15 @@ export default class Cards extends Component{
             <div className="container">
                 <div className="second__c">
                     <div className="title_container">Cards</div>
+                    <div className=" title2__">Choice favorite color and looking</div>
                     <div className="cards__b">
                         <div id="big-block">
+                            <div class="small-block orange">...</div>
+                            <div class="small-block yell">...</div>
+                            <div class="small-block blue">...</div>
+                            <div class="small-block purple">...</div>
                             <div class="small-block">...</div>
-                            <div class="small-block">...</div>
-                            <div class="small-block">...</div>
-                            <div class="small-block">...</div>
-                            <div class="small-block">...</div>
-                            <div class="small-block ">...</div>
+                            <div class="small-block green">...</div>
                         </div>
                     </div>
                 </div>
