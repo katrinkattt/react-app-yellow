ee717172417e13a308fe461b60f80c683915e1a4
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-16 21:08:19 +0500

    pers
    пока так потом в отдельные коипооненты переопределю

--- app-lending/yell-app/src/components/command.jsx
+++ app-lending/yell-app/src/components/command.jsx
@@ -3,7 +3,14 @@ import React, {Component} from 'react';
 export default class Command extends Component {
     render(){
         return(
-            <div className="container"></div>
+            <div className="container">
+                <div className="command__main">
+                    <div className="person"></div>
+                    <div className="person"></div>
+                    <div className="person"></div>
+                    <div className="person"></div>
+                </div>
+            </div>
         )
     }
 }
