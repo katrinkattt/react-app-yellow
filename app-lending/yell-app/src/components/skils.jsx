3b1e8d8429d195c83aaeb71e75030beaad7f1b1e
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-16 20:57:51 +0500

    st

--- app-lending/yell-app/src/components/skils.jsx
+++ app-lending/yell-app/src/components/skils.jsx
@@ -6,9 +6,10 @@ export default class Skils extends Component {
             <div className="container">
                 <div className="skils__main">
                     <div className="skils__about">
-                        <div className="skils__title">Our skils</div>
-                        <div className="skils__text">
-
+                        <div className=" title_container skils__title">Our skils</div>
+                        <div className="title2__ skils__text">
+                        Теперь переходим к File / Export и экспортируем все артборды в PNG (1x, 2x, 3x) или в SVG формате. Таким образом, у вас каждый объект будет отдельным файлом к большой радости разработчика.
+                        Правда я заметил одну проблему: при экспорте к SVG файлу добавляется квадратный фон (хотя я удаляю заливку фона). Надеюсь, что это баг и Adobe его скоро пофиксит.
                         </div>
                     </div>
                     <div className="skils__foto"></div>
