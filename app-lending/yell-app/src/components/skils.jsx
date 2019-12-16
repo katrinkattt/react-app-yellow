dfebdd547ff2a2c15672f18f8717fdf8991b7912
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-16 20:35:27 +0500

    structure

--- app-lending/yell-app/src/components/skils.jsx
+++ app-lending/yell-app/src/components/skils.jsx
@@ -3,7 +3,17 @@ import React, {Component} from 'react';
 export default class Skils extends Component {
     render(){
         return(
-            
+            <div className="container">
+                <div className="skils__main">
+                    <div className="skils__about">
+                        <div className="skils__title">Our skils</div>
+                        <div className="skils__text">
+
+                        </div>
+                    </div>
+                    <div className="skils__foto"></div>
+                </div>
+            </div>
         )
     }
 }
