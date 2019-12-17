334a9c4806558fd4ba5c26a9e7b1f2472737bab1
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-17 20:32:54 +0500

    blocks

--- app-lending/yell-app/src/components/serviceOffer.jsx
+++ app-lending/yell-app/src/components/serviceOffer.jsx
@@ -5,9 +5,11 @@ export default class ServiceOffer extends Component {
         return(
             <div className="offer__main">
                 <div className="offer__img"></div>
-                <div className="offer__title">offer</div>
-                <div className="offer__more">it's offer to be best in market place</div>
-            </div>
+                <div className="offer__info">
+                    <div className="offer__title">offer</div>
+                    <div className="offer__more">it's offer to be best in market place</div>
+                </div>
+                </div>
         )
     }
 }
