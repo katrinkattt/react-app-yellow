22efd0bdbef81e11fe2d48e257d89ba93225f2dd
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-17 21:15:40 +0500

    init btn

---
+++ app-lending/yell-app/src/components/btn.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Btn extends Component {
+    render(){
+        return(
+            <div className="btn__main">
+                <div className="text__btn">
+                    <a href="#files">READ MORE</a>
+                </div>
+            </div>
+        )
+    }
+}
