8900bcc58d40393d2f46297546742693a302c7cd
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-17 22:25:11 +0500

    link

--- app-lending/yell-app/src/components/btn.jsx
+++ app-lending/yell-app/src/components/btn.jsx
@@ -4,9 +4,9 @@ export default class Btn extends Component {
     render(){
         return(
             <div className="btn__main">
-                <div className="text__btn">
-                    <a href="#files">READ MORE</a>
-                </div>
+                <a href="#files" className="text__btn">
+                    <span>READ MORE</span>
+                </a>
             </div>
         )
     }
