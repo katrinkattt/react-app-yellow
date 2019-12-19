f5d7dbff5a57f5ba00fd22cc10108eaba4723cc9
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-19 19:52:00 +0500

    constructor

--- app-lending/yell-app/src/components/Cards.jsx
+++ app-lending/yell-app/src/components/Cards.jsx
@@ -1,6 +1,25 @@
 import React, {Component} from 'react';
 
 export default class Cards extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            curColor: 'white',
+            colors: [
+                'red',
+                'blue',
+                'black',
+                'white',
+                'aqua',
+                'green',
+            ]
+        }
+    }
+    changeColor(color){
+        this.setState({
+            curColor: color
+        })
+    }
     render(){
         return(
             <div className="container">
