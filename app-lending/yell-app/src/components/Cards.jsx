beb4f87a5ee4b4b3e294c0541037e2205f0f4fc5
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-20 19:44:41 +0500

    img

--- app-lending/yell-app/src/components/Cards.jsx
+++ app-lending/yell-app/src/components/Cards.jsx
@@ -41,7 +41,8 @@ export default class Cards extends Component{
                                 this.state.colors.map((color, id) =>
                                 <div className="small-block"
                                     key={id}
-                                    style={{background:{color}}}>
+                                    style={{background:{color}}}
+                                >
                                 </div>
                                 )
                             }
