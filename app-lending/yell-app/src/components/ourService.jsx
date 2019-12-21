c8ae747c68074f1c9fbd472217619da185380ce4
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-21 16:47:07 +0500

    reorganization

--- app-lending/yell-app/src/components/ourService.jsx
+++ app-lending/yell-app/src/components/ourService.jsx
@@ -2,6 +2,31 @@ import React, {Component} from 'react';
 import ServiceOffer from './serviceOffer';
 
 export default class OurService extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            img: [
+                'workwork'
+            ],
+            title: ['offer1', 'offer2', 'offer3'],
+            text: [
+                '  display block height width background-color position: absolute.',
+                'background-color position: absolute v background-color position: absolute.',
+                'display block height width 3 display block height width.'
+            ]
+        }
+    }
+    changeText(text){
+        this.setState({
+            curText: text
+        })
+    }
+    changeTitle(title){
+        this.setState({
+            curTitle: title
+        });
+
+    }
     render(){
         return(
             <div className="container">
@@ -9,15 +34,35 @@ export default class OurService extends Component {
                     <div className="title_container">Our Services</div>
                     <div className=" title2__">Use the colspan and rowspan attribute to let the content span over multiple columns or rows.</div>
                     <div id="big-block">
+                        <div className="serv__b"><ServiceOffer
+                        {
+                            ...this.state.title.map((title, text, id) =>
+                            <div className="offer__main">
+                                <div className="offer__img">
+                                <div className="offer__info" >
+                                    <div className="offer__title"
+                                        key={id}
+                                        value={()=> {this.changeTitle({t:title})}}
+                                    ></div>
+                                    <div className="offer__text"
+                                        key={id}
+                                        value={()=> {this.changeText({t:text})}}
+                                    ></div>
+                                </div>
+                            </div>
+                            </div>
+                            )
+                        }
+                        /></div>
+                    </div>
+                        {/* <div className="serv__b"><ServiceOffer /></div>
                         <div className="serv__b"><ServiceOffer /></div>
                         <div className="serv__b"><ServiceOffer /></div>
                         <div className="serv__b"><ServiceOffer /></div>
-                        <div className="serv__b"><ServiceOffer /></div>
-                        <div className="serv__b"><ServiceOffer /></div>
-                        <div className="serv__b"><ServiceOffer /></div>
+                        <div className="serv__b"><ServiceOffer /></div> */}
                     </div>
                 </div>
-            </div>
+            // </div>
         )
     }
 }
