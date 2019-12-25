626bf1bf0b7baa1b5850a3722a3e7c4d49f80f93
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-25 17:37:40 +0500

    Update App.js

--- app-lending/yell-app/src/App.js
+++ app-lending/yell-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import MainContainer from './components/mainContainer';
 import OurService from './components/ourService';
 import Cards from './components/Cards';
