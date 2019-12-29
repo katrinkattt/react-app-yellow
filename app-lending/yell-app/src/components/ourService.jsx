5a68a850e02a9011913ef241a19479c66b4a3967
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-29 23:23:00 +0500

    add img im array
    map надо заменить на for лучше

--- app-lending/yell-app/src/components/ourService.jsx
+++ app-lending/yell-app/src/components/ourService.jsx
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 // import ServiceOffer from './serviceOffer';
+import icon11 from '../media/icon11.png';
+import icon12 from '../media/icon12.png';
+import icon13 from '../media/icon13.png';
+import icon21 from '../media/icon21.png';
+import icon22 from '../media/icon22.png';
+import icon23 from '../media/icon23.png';
 
 export default class OurService extends Component {
     constructor(props){
         super(props)
         this.servState = {
             img: [
-                'workwork'
+                icon11,
+                icon12,
+                icon13,
+                icon21,
+                icon22,
+                icon23
+                
             ],
             title: ['offer1', 'offer2', 'offer3', 'offer4', 'offer5', 'offer6'],
             text: [
@@ -29,18 +41,22 @@ export default class OurService extends Component {
                     <div className=" title2__">Use the colspan and rowspan attribute to let the content span over multiple columns or rows.</div>
                     <div id="big-block">
                             {
-                                this.servState.title.map((titl, id) =>
-                                    this.servState.text.map((txt, id) =>
+                               this.servState.img.map((png, id) =>
+                                    this.servState.title.map((titl, id) =>
+                                        this.servState.text.map((txt, inn) =>
                                         <div className="serv__b">
                                             <div className="offer__main">
-                                                <div className="offer__img"></div>
+                                                <div className="offer__img"
+                                                    key={id}
+                                                    style={{background:`url(${png}) no-repeat`}}
+                                                ></div>
                                                 <div className="offer__info" >
                                                     <div className="offer__title"> {titl}</div>
                                                     <div className="offer__more">{txt}</div>
                                                 </div>
                                             </div>
                                         </div>
-                                ))
+                                )))
                             }
                         </div>
                 </div>
