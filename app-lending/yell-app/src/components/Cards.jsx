93c3e96293c42a564ca72fdde4829f3bd3650a41
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-29 20:41:28 +0500

    add render text in 2 block

--- app-lending/yell-app/src/components/Cards.jsx
+++ app-lending/yell-app/src/components/Cards.jsx
@@ -8,6 +8,11 @@ import bg4 from '../media/bg4.png';
 import bg5 from '../media/bg5.png';
 import replace from '../media/replace_image.png';
 
+var block__2 = document.getElementById("card__2");
+var divBlock2Content = document.createElement("div");
+divBlock2Content.className = "text__card2";
+var txt2B = document.getElementsByClassName("text__card2");
+txt2B.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem"
 
 
 export default class Cards extends Component{
@@ -36,14 +41,13 @@ export default class Cards extends Component{
                         <div id="big-block">
                             {
                                 this.state.colors.map((color, id) =>
-                                <div className="small-block"
+                                <div className="small-block" id={"card__"+id}
                                     key={id}
                                     style={{background:`url(${color}) no-repeat `}}
                                 >
                                 </div>
                                 )
                             }
-                            
                         </div>
                     </div>
                 </div>
@@ -51,3 +55,4 @@ export default class Cards extends Component{
         )
     }
 }
+
