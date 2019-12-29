3d8cbc4d90951d7b16c8730a0e3c7520b9fa47b7
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-29 20:12:10 +0500

    setting style

--- app-lending/yell-app/src/components/Cards.jsx
+++ app-lending/yell-app/src/components/Cards.jsx
@@ -14,7 +14,7 @@ export default class Cards extends Component{
     constructor(props){
         super(props)
         this.state = {
-            curColor: 'white',
+            curColor: '#fcd708',
             colors: [
                 bg1,
                 bg,
