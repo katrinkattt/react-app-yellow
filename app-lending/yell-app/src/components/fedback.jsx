99dd2aed16c01cd8d3b8b648a5feccdbf14440d0
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-30 23:57:32 +0500

    init

---
+++ app-lending/yell-app/src/components/fedback.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Feedback extends Component{
+    render(){
+        return(
+            <div className="feedback">
+                
+            </div>
+        )
+    }
+}
