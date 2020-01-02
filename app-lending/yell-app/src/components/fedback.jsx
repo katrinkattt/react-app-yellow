186e9cdbf0be2fed842d17b4417e9565d01a9c65
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-02 20:49:43 +0500

    add text for feedback

--- app-lending/yell-app/src/components/fedback.jsx
+++ app-lending/yell-app/src/components/fedback.jsx
@@ -5,7 +5,9 @@ export default class Feedback extends Component{
         return(
             <div className="feedback">
                 <div className="feed__block">
-                    <div className="title__feed">Suscribe to our Newsletter</div>
+                    <div className="title__feed">Suscribe to our Newsletter
+                    <div className="feed__text">Subscibe to our newsletter and get 30% off free downloads mockups</div>
+                    </div>
                     <div className="feedback__inputs">
                         <input className="feed__input" type="text" id="email" placeholder="enter your email"/>
                         <input type="submit" className="feed__btn" id="send" value="SUBSCRIBE"/>
