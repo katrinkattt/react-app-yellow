238afc82cb58b84b1a17608252403da98cc9b19d
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-02 20:24:53 +0500

    feedback add value

--- app-lending/yell-app/src/components/fedback.jsx
+++ app-lending/yell-app/src/components/fedback.jsx
@@ -4,7 +4,11 @@ export default class Feedback extends Component{
     render(){
         return(
             <div className="feedback">
-                
+                <div className="feed__block">
+                    <div className="title__feed">Suscribe to our Newsletter</div>
+                    <input className="feed__input" type="text" id="email" placeholder="enter your email"/>
+                    <input type="submit" className="feed__btn" id="send" value="SUBSCRIBE"/>
+                </div>
             </div>
         )
     }
