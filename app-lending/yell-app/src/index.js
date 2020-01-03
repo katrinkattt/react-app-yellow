3eb52c8df8484999c17c3fa447a8daa766487115
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-03 23:25:12 +0500

    Update index.js

--- app-lending/yell-app/src/index.js
+++ app-lending/yell-app/src/index.js
@@ -5,19 +5,17 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-this.state = {
-    email:'',
-    isReady:{
-        email:false
-    },
-    // input: document.getElementById('email'),
-    // patternEmail: /(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
-};
-const email = document.getElementById('email');
+const state = {
+        email:'',
+        isReady:{
+            email:false
+        }
+    };
 const patternEmail =/(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    email.addEventListener('keyup', (e) =>{
-    this.state.email = e.currentTarget.value;
-    this.state.isReady.email = this.state.patternEmail.test(this.state.email);
+const email = document.getElementById('email');
+email.addEventListener('keyup', (e) =>{
+    state.email = e.currentTarget.value;
+    state.isReady.email = state.patternEmail.test(state.email);
 })
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
