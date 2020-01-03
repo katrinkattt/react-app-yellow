a3932bd94288123ffdf030aac2e71f5ce77a5bdc
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-03 22:05:01 +0500

    add links for png

--- app-lending/yell-app/src/components/social.jsx
+++ app-lending/yell-app/src/components/social.jsx
@@ -7,14 +7,20 @@ export default class Social extends Component {
     render(){
         return(
             <div className="socials">
-                <div className="social">
-                    <img src={`${facebook}`} alt=""/>
+                <div className="social" >
+                    <a href="https://www.facebook.com/">
+                        <img src={`${facebook}`}  alt=""/></a>
                 </div>
                 <div className="social">
-                    <img src={`${twitt}`} alt=""/>
+                    <a href="https://twitter.com/">
+                        <img src={`${twitt}`} alt=""/>
+                    </a>
                 </div>
                 <div className="social">
-                    <img src={`${inst}`} alt=""/>
+                    <a href="https://www.instagram.com/">
+                        <img src={`${inst}`} alt=""/>
+                    </a>
+                    
                 </div>
             </div>
         )
