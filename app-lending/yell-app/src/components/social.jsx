1295f8122dd3be4bd9b841e6ff18469dfb1ffa26
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-03 21:07:21 +0500

    add in progect

--- app-lending/yell-app/src/components/social.jsx
+++ app-lending/yell-app/src/components/social.jsx
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
+import facebook from '../media/facebook.png';
+import twitt from '../media/twitter.png';
+import inst from '../media/instagram.png'
 
 export default class Social extends Component {
     render(){
         return(
             <div className="socials">
                 <div className="social">
-                    <img src="#" alt=""/>
+                    <img src={`${facebook}`} alt=""/>
                 </div>
                 <div className="social">
-                    <img src="#" alt=""/>
+                    <img src={`${twitt}`} alt=""/>
                 </div>
                 <div className="social">
-                    <img src="#" alt=""/>
+                    <img src={`${inst}`} alt=""/>
                 </div>
             </div>
         )
