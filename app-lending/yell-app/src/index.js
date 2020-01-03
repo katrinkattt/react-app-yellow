1d6d83375b26d7609f1954626ea86e93887f2880
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-03 23:18:02 +0500

    validator for email

--- app-lending/yell-app/src/index.js
+++ app-lending/yell-app/src/index.js
@@ -5,7 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+this.state = {
+    email:'',
+    isReady:{
+        email:false
+    },
+    // input: document.getElementById('email'),
+    // patternEmail: /(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+};
+const email = document.getElementById('email');
+const patternEmail =/(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    email.addEventListener('keyup', (e) =>{
+    this.state.email = e.currentTarget.value;
+    this.state.isReady.email = this.state.patternEmail.test(this.state.email);
+})
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
