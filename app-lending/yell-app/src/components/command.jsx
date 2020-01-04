f6e494d81384617626ab9cc266221c33e28d1145
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-04 22:12:36 +0500

    add img in progect

--- app-lending/yell-app/src/components/command.jsx
+++ app-lending/yell-app/src/components/command.jsx
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
+import team1 from '../media/team1.png';
+import team2 from '../media/team2.png';
+import team3 from '../media/team3.png';
+import team4 from '../media/team4.png';
 
 export default class Command extends Component {
+    constructor(props){
+        super(props)
+        this.teamstate = {
+            img:[
+                team1,
+                team2,
+                team3,
+                team4
+            ],
+            
+        }
+    }
     render(){
         return(
             <div className="container">
                 <div className="title_container">Our command</div>
                 <div className="command__main">
-                    {/* <div className="person"></div>
                     <div className="person"></div>
-                    <div className="person"></div>      DODELAT'
-                    <div className="person"></div> */}
                 </div>
             </div>
         )
