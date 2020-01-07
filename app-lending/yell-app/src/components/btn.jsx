086186fa0619d954d4cb6352eb601041087c99bd
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-07 19:30:53 +0500

    Update btn.jsx

--- app-lending/yell-app/src/components/btn.jsx
+++ app-lending/yell-app/src/components/btn.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 
 export default function Btn(props) {
-        return(
-            <div className="btn__main">
-                <a href="#files" className="btn__main">
-                    <span className="text__btn">{props.title}</span>
-                </a>
-            </div>
-        )
-    }
+    return(
+        <div className="btn__main">
+            <a href="#files" className="btn__main">
+                <span className="text__btn">{props.title}</span>
+            </a>
+        </div>
+    )
+}
 
