27a06bc4d862960c42e0f94db20d089d42540207
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-08 15:47:01 +0500

    Update index.js

--- app-lending/yell-app/src/index.js
+++ app-lending/yell-app/src/index.js
@@ -26,6 +26,7 @@ btn.addEventListener('click', (e) =>{
     }
 })
 
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
